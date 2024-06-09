a8109db3d8b4798f82e44dea9b5eaaeeda314ba7
NinaZurash <nino.zurashvili.nz@gmail.com>
2024-06-09 23:22:24 +0400

    init

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
 import morgan from "morgan";
+import { prisma } from "./lib/prisma";
 
 dotenv.config();
 const app = express();
+
+async function main() {
+  const allUsers = await prisma.user.findMany();
+  console.log(allUsers);
+}
 
 const PORT = process.env.PORT || 3000;
 
