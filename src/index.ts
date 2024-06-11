d96bb3852eb4980d027379849d789d0fc7fbe07e
NinaZurash <nino.zurashvili.nz@gmail.com>
2024-06-11 10:19:25 +0400

    implemented prisma model

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,18 @@
-import express from "express";
+import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 import morgan from "morgan";
 import { prisma } from "./lib/prisma";
+import { rootRouter } from "./routes";
 
 dotenv.config();
-const app = express();
-
-async function main() {
-  const allUsers = await prisma.user.findMany();
-  console.log(allUsers);
-}
+const app: Express = express();
 
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(morgan("tiny"));
 
-app.get("/", (_, res) => {
-  res.status(200).json({ message: "ok" });
-});
+app.use("/api", rootRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
