c4b17fd7ac093c030c5e431021c598d1ab21bde2
nzurashv <nino.zurashvili@cern.ch>
2024-06-28 18:42:35 +0400

    implemented error handlers

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express, { Express } from "express";
 import morgan from "morgan";
 import { rootRouter } from "./routes";
 import { PORT } from "./config";
+import { errorHandler } from "./middlewares/errorHandler";
 
 const app: Express = express();
 
@@ -9,6 +10,8 @@ app.use(express.json());
 app.use(morgan("tiny"));
 
 app.use("/api", rootRouter);
+
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
